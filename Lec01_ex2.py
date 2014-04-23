4628040f1949acd8eb27a45ab722cd298cc7b059
marchuqq <marchuqq@gmail.com>
2014-04-23 22:12:17 +0800

    Lec01All

---
+++ Lec01_ex2.py
@@ -0,0 +1,6 @@
+x = float(input("Please enter the first number: "));
+y = float(input("Please enter the second number: "));
+print("the first number is ", x);
+print("the second number is ", y);
+print("the result is "+str(x*y));
+input("");
